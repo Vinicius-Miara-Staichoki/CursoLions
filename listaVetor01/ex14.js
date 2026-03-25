46176493981ca57aceb167ee60cec4b414d5f951
Vinicius-Miara-Staichoki <staichokivini@gmail.com>
2026-03-25 17:05:07 -0300

    ex14 listaVetor01

---
+++ listaVetor01/ex14.js
@@ -0,0 +1,10 @@
+let nums = [11, 12, 13, 14, 15, 16, 17, 18]
+let x = 15;
+for (i = 0; i < nums.length; i++) {
+    if (x === nums[i]) {
+        console.log(i)
+    }
+    else { console.log(-1) }
+
+}
+
