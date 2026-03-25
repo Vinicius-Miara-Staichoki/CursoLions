98e1c7af1007be2270cee36870440671382f6b20
Vinicius-Miara-Staichoki <staichokivini@gmail.com>
2026-03-25 17:00:18 -0300

    ex13 listaVetor01

---
+++ listaVetor01/ex13.js
@@ -0,0 +1,17 @@
+let nums = [1, 2, 3, 4, 5, 1, 1, 9, 0, 1]
+let x = 1;
+let soma = 0;
+
+
+for (i = 0; i < nums.length; i++) {
+    if (x === nums[i]) {
+        soma++
+    }
+
+
+
+
+
+}
+
+console.log(soma)
