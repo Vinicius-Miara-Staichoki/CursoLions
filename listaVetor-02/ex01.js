b95bde345c0f196320f1092e652e286bf80f083a
Vinicius-Miara-Staichoki <staichokivini@gmail.com>
2026-03-26 15:43:15 -0300

    ex01 corrigido da listaVetor02

--- listaVetor-02/ex01.js
+++ listaVetor-02/ex01.js
@@ -1,4 +1,4 @@
-let nums = [1, 2, 4, 300, 5, 78, 9, 81];
+let nums = [1, 2, 4, 300, 5, 78, 300, 81];
 let maior2 = nums[0];
 let maior = nums[0];
 let maior3 = nums[0];
@@ -8,14 +8,28 @@ for (i = 0; i < nums.length; i++) {
         maior = nums[i];
 
     }
+
     else {
-        if (maior2 < nums[i]){
-            maior3 = maior2;
-            maior2 = nums[i];
+
+    }
+    if (maior2 < nums[i]) {
+        maior3 = maior2;
+        maior2 = nums[i];
+        if (maior2 === maior) {
+            maior2 = maior3;
         }
     }
 
+}
+
+
+
+if (maior > maior2) {
+    console.log("Maior: ", maior);
+    console.log("Segundo maior: ", maior2);
+}
+if (maior2 === maior) {
+    console.log("Maior: ", maior)
+    console.log("Segundo maior: ", maior3)
 
 }
-console.log("Maior: ", maior);
-console.log("Segundo maior: ", maior2);
